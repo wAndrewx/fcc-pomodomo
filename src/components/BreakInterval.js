39b8af8cee75030c4791840b6d63314dde2d8bc1
wAndrewx <andrew.huynh17@gmail.com>
2021-04-26 19:52:03 -0400

    Modules functional

--- src/components/BreakInterval.js
+++ src/components/BreakInterval.js
@@ -1,6 +1,29 @@
-import { chakra,Button,HStack } from "@chakra-ui/react";
+import { Button, HStack, Text } from "@chakra-ui/react";
 
 
-export const BreakButtons = (props)=>{
-    
+export const BreakButtons = (props) => {
+
+    const handleTime = (increaseOrDecrease) => {
+
+        if (increaseOrDecrease === true) {
+            props.breakStateTime++;
+        } else {
+            props.breakStateTime--;
+        }
+    }
+    return (
+        <div>
+            <HStack>
+                <Button
+                    id="break-increment" onClick={() => { handleTime(true) }}> Increase </Button>
+                <Text id="break-label">
+                    Break Length
+            <Text id="break-length">{props.sessionMinute}</Text>
+                </Text>
+
+                <Button id="break-decrement" onClick={() => { handleTime(false) }}> Decrease </Button>
+            </HStack>
+        </div>
+    )
+
 }
--- src/components/sessionInterval.js
+++ src/components/sessionInterval.js
@@ -1,8 +1,23 @@
-import { chakra,Button,HStack } from "@chakra-ui/react";
+import { chakra, Button, HStack, Text } from "@chakra-ui/react";
 
+//handling time function in parent
+export const SessionButtons = (props) => {
+    const sessionStateTime = (incOrDec) => {
+        props.sessionStateTime(incOrDec);
+    }
 
-export const SessionButtons=(props)=>{
-    
+    return (<div>
+        <HStack>
+            <Button
+                id="session-increment" onClick={() => { sessionStateTime(true) }}> Increase </Button>
+            <Text id="session-label">
+                Session Length
+            <Text id="session-length">{props.sessionMinute}</Text>
+            </Text>
+
+            <Button id="session-decrement" onClick={() => { props.sessionStateTime(false); }} >Decrease </Button>
+        </HStack>
+    </div>)
 }
 
 
