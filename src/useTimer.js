23d72bd23e072f59acc8d1bd436e5a398a172fc4
wAndrewx <andrew.huynh17@gmail.com>
2021-04-25 21:25:05 -0400

    splitting code into components

--- src/useTimer.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-
-export const useTimer = (initTime) => {
-    const [timer, setTimer] = useState(initTime);
-
-    return [timer,
-        e => { setTimer(...timer, timer + 1) }]
-}
